e58f8c6435800a6a23de7a106a62f5a17165a4c5
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-23 12:38:36 -0700

    Changed Main to oldMain and named the new files method Main.

---
+++ MyFirstCPP/Runner.cpp
@@ -0,0 +1,13 @@
+//
+//  Runner.cpp
+//  MyFirstCPP
+//
+//  Created by Woolsey, Adam on 1/23/18.
+//  Copyright © 2018 Woolsey, Adam. All rights reserved.
+//
+
+int main()
+{
+    
+    return 0;
+}
